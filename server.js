e7aa44f92dae76400e55b690796d7938f8e7be0c
Muhammad Manekia <muhammadmanekia0@gmail.com>
2024-07-05 18:33:06 -0500

    .env added

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = 'mongodb+srv://muhammadmanekia0:gCAFO1GUDxgyurSD@cluster0.dmhysd1.mongodb.net/HalalApp?retryWrites=true&w=majority&appName=Cluster0';
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+const mongoURI = process.env.MONGO_URI;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const restaurantSchema = new mongoose.Schema({
   name: String,
@@ -20,27 +22,35 @@ const restaurantSchema = new mongoose.Schema({
   categories: Array,
   coordinates: Object,
   quickFacts: Array,
-  time: String
+  time: String,
 });
 
 const categoryImageSchema = new mongoose.Schema({}, { strict: false });
 
-const Restaurant = mongoose.model('Restaurant', restaurantSchema, 'Restaurants');
-const CategoryImage = mongoose.model('CategoryImages', categoryImageSchema, "CategoryImages");
+const Restaurant = mongoose.model(
+  "Restaurant",
+  restaurantSchema,
+  "Restaurants"
+);
+const CategoryImage = mongoose.model(
+  "CategoryImages",
+  categoryImageSchema,
+  "CategoryImages"
+);
 
-app.get('/category-images', async (req, res) => {
-    try {
-      const categoryImages = await CategoryImage.findOne({});
-      res.json(categoryImages);
-    } catch (error) {
-      console.error("Error fetching category images:", error);
-      res.status(500).send("Error fetching category images");
-    }
-  });
-
-app.get('/restaurants', async (req, res) => {
+app.get("/category-images", async (req, res) => {
   try {
-    const restaurants = await Restaurant.find(); 
+    const categoryImages = await CategoryImage.findOne({});
+    res.json(categoryImages);
+  } catch (error) {
+    console.error("Error fetching category images:", error);
+    res.status(500).send("Error fetching category images");
+  }
+});
+
+app.get("/restaurants", async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find();
     res.json(restaurants);
   } catch (error) {
     res.status(500).send(error);
