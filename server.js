1da1ab85fbc78ce2504dfa4b689435d829393e30
Muhammad Manekia <muhammadmanekia0@gmail.com>
2024-07-05 18:17:41 -0500

    Push for server

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const mongoURI = 'mongodb+srv://muhammadmanekia0:gCAFO1GUDxgyurSD@cluster0.dmhysd1.mongodb.net/HalalApp?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const restaurantSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  image_url: String,
+  rating: String,
+  price: String,
+  categories: Array,
+  coordinates: Object,
+  quickFacts: Array,
+  time: String
+});
+
+const categoryImageSchema = new mongoose.Schema({}, { strict: false });
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema, 'Restaurants');
+const CategoryImage = mongoose.model('CategoryImages', categoryImageSchema, "CategoryImages");
+
+app.get('/category-images', async (req, res) => {
+    try {
+      const categoryImages = await CategoryImage.findOne({});
+      res.json(categoryImages);
+    } catch (error) {
+      console.error("Error fetching category images:", error);
+      res.status(500).send("Error fetching category images");
+    }
+  });
+
+app.get('/restaurants', async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find(); 
+    res.json(restaurants);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
