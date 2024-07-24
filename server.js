6ba0227af9650d0cedb398aabb4f945156152e24
Muhammad Manekia <muhammadmanekia0@gmail.com>
2024-07-23 22:10:52 -0500

    review addition

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ mongoose
 const reviewSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   rating: { type: Number, required: true, min: 1, max: 5 },
+  title: { type: String, required: true },
   comment: { type: String, required: true },
   date: { type: Date, default: Date.now },
 });
