b764dd56b73f20f30c3a7e50b155182f18264c91
Muhammad Manekia <muhammadmanekia0@gmail.com>
2024-10-05 15:16:16 -0500

    salt error

--- utils/encryptionUtils.js
+++ utils/encryptionUtils.js
@@ -1,31 +1,16 @@
 const cryptoJS = require("crypto-js");
 
 exports.encryptData = (data, secretKey) => {
-  try {
-    // Ensure the data is valid JSON
-    return cryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString();
-  } catch (error) {
-    console.error("Encryption failed:", error.message);
-    throw new Error("Encryption error");
+  if (data) {
+    return cryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString(
+      cryptoJS.enc.Base64
+    );
   }
 };
 
 exports.decryptData = (encryptedData, secretKey) => {
-  try {
-    if (!encryptedData || typeof encryptedData !== "string") {
-      throw new Error("Invalid encrypted data format");
-    }
-
+  if (encryptedData) {
     const bytes = cryptoJS.AES.decrypt(encryptedData, secretKey);
-    const decryptedText = bytes.toString(cryptoJS.enc.Utf8);
-
-    if (!decryptedText) {
-      throw new Error("Decryption failed, invalid secret key or data.");
-    }
-
-    return JSON.parse(decryptedText);
-  } catch (error) {
-    console.error("Decryption failed:", error.message);
-    throw new Error("Decryption error");
+    return JSON.parse(bytes.toString(cryptoJS.enc.Base64));
   }
 };
