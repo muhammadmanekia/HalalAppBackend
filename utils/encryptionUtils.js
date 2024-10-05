97d53238713cfeda191aed93953acf18d44ef520
Muhammad Manekia <muhammadmanekia0@gmail.com>
2024-10-05 14:41:35 -0500

    salt error

--- utils/encryptionUtils.js
+++ utils/encryptionUtils.js
@@ -1,10 +1,14 @@
 const cryptoJS = require("crypto-js");
 
 exports.encryptData = (data, secretKey) => {
-  return cryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString();
+  if (data) {
+    return cryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString();
+  }
 };
 
 exports.decryptData = (encryptedData, secretKey) => {
-  const bytes = cryptoJS.AES.decrypt(encryptedData, secretKey);
-  return JSON.parse(bytes.toString(cryptoJS.enc.Utf8));
+  if (encryptedData) {
+    const bytes = cryptoJS.AES.decrypt(encryptedData, secretKey);
+    return JSON.parse(bytes.toString(cryptoJS.enc.Utf8));
+  }
 };
