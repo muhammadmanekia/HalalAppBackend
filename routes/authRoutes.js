695a832a284246a72d2b615e1f91da40a1efa74b
Muhammad Manekia <muhammadmanekia0@gmail.com>
2024-10-13 22:39:14 -0500

    added forgot password and delete

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/authController");
+const {
+  register,
+  login,
+  deleteAccount,
+  forgotPassword,
+  resetPassword,
+  verifyResetCode,
+} = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
-
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", verifyResetCode);
+router.delete("/delete/:userId", deleteAccount);
 module.exports = router;
