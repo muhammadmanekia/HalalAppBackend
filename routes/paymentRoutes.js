db06c06f75c91101bfcb73f852778cb5e4aa500a
Muhammad Manekia <muhammadmanekia0@gmail.com>
2025-05-19 22:16:49 -0500

    Revert "paymentAdded"
    This reverts commit 8e4cb3aa5c094a125a6abd354ee10a1f585c1a5d.

--- routes/paymentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { sendPayment } = require("../controllers/paymentController");
-
-router.post("/", sendPayment);
-
-module.exports = router;
