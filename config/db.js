777e9a7602d1e5c8485605592a587462491e742f
Muhammad Manekia <muhammadmanekia0@gmail.com>
2025-10-28 17:49:36 -0500

    notif

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoURI = process.env.MONGO_URI;
 
 const connectDB = async () => {
   mongoose
-    .connect(mongoURI)
+    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
 };
