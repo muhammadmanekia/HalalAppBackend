1c805b3ea8b4b6be9dfd752a55acae865de9d291
Muhammad Manekia <muhammadmanekia0@gmail.com>
2025-10-28 19:44:46 -0500

    feedback update

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -13,13 +13,39 @@ exports.postAppFeedback = async (req, res) => {
 };
 
 exports.postRestaurantFeedback = async (req, res) => {
-  const { user, feedback } = req.body;
+  const { user, feedback, restaurantId, restaurantName, restaurantAddress } =
+    req.body;
 
   try {
-    const restaurantFeedback = new RestaurantFeedback({ user, feedback });
+    const restaurantFeedback = new RestaurantFeedback({
+      user,
+      feedback,
+      restaurantId,
+      restaurantName,
+      restaurantAddress,
+    });
     await restaurantFeedback.save();
     res.status(201).json({ message: "Feedback submitted successfully" });
   } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getRestaurantFeedback = async (req, res) => {
+  const { restaurantId } = req.params; // Assuming restaurantId is passed as a URL parameter
+
+  try {
+    const feedbacks = await RestaurantFeedback.find({ restaurantId });
+
+    if (!feedbacks.length) {
+      return res
+        .status(404)
+        .json({ message: "No feedback found for this restaurant" });
+    }
+
+    res.status(200).json(feedbacks);
+  } catch (error) {
+    console.log(error);
     res.status(500).json({ error: error.message });
   }
 };
